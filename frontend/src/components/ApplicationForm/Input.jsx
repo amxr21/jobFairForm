71809c2c6c1629056d9f7d61a90eddad04f54424
amxr21 <ammar211080@gmail.com>
2024-05-27 23:22:36 +0400

    Update Input.jsx

--- frontend/src/components/ApplicationForm/Input.jsx
+++ frontend/src/components/ApplicationForm/Input.jsx
@@ -26,7 +26,7 @@ const Input = ({label, type, name}) => {
     return (
         <div className="flex flex-col grow mb-4 md:my-0">
             <h2 className="text-md md:text-lg mb-2">{label} :-</h2>
-            <input ref={refLabel} onChange={getInput} type={type} name={name} className="bg-transparent border border-gray-700 rounded-lg py-1.5 px-2" placeholder={label} max={max} />
+            <input ref={refLabel} onChange={getInput} type={type} name={name} className="w-full md:w-fit bg-transparent border border-gray-700 rounded-lg py-1.5 px-2" placeholder={label} max={max} />
         </div>
     )
 }
