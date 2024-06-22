1f29d3e09727bc2ca0351b3960843de78079c9c7
amxr21 <ammar211080@gmail.com>
2024-06-22 23:24:02 +0400

    Update MainBanner.jsx

--- frontend/src/components/ApplicationForm/MainBanner.jsx
+++ frontend/src/components/ApplicationForm/MainBanner.jsx
@@ -24,11 +24,11 @@ const MainBanner = () => {
     return (
         <div id="Hero" className="md:w-8/12 md:mx-auto my-4 md:my-12 py-8 md:py-12 h-fit overflow-hidden">
             <div className="md:text-2xl md:text-left mb-4">
-                <h2 className="md:text-6xl leading-[1.2em] text-7xl font-bold mb-4 md:mb-2">Hala Wallah</h2>
-                <p className="text-2xl">Start filling your first Job Resume !!</p>
+                <h2 className="md:text-6xl leading-[1.2em] text-7xl font-bold mb-4 md:mb-2">Welcome to your first Job Fair!</h2>
+                <p className="text-2xl">Connect with top employers, discover exciting career opportunities, and take the next step in your professional journey</p>
             </div>
             <div className="start-button flex md:mt-0 mt-12">
-                <button ref={startButton} onClick={startForm} className="w-full py-4 px-3 border border-black rounded-xl text-3xl">Start</button>
+                <button ref={startButton} onClick={startForm} className="w-full py-4 px-3 border border-black rounded-xl text-3xl">Lets Start</button>
             </div>
         </div>
     )
