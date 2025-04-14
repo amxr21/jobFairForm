84c2b1c34c29ac3e53164c95090889cdfb27c537
amxr21 <ammar211080@gmail.com>
2025-04-14 15:03:29 +0400

    upload testing file

--- frontend/src/components/SuccessPage/ConfirmMessage.jsx
+++ frontend/src/components/SuccessPage/ConfirmMessage.jsx
@@ -5,8 +5,8 @@ const ConfirmMessage = ({ emoji }) => {
                 <img src={emoji} alt="" />
             </div>
             <div className="text italic flex flex-col gap-2">
-                <h2 className="text-3xl md:text-6xl font-bold">You are all Set!</h2>
-                <p>See you on Tuesday, 22nd April at M11 from 10:00AM-02:00PM</p>
+                <h2 className="text-3xl md:text-6xl font-bold">You are all set!</h2>
+                <p>See you on 22nd April 2025, Tuesday at M11 from 10:00 AM till 02:00 PM</p>
             </div>
         </div>
     )
