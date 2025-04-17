4bad306e71de7cb55a48dbb3863258aacd4df57c
amxr21 <ammar211080@gmail.com>
2025-04-17 06:21:14 +0400

    update technical skills

--- frontend/src/components/ApplicationForm/Experiences.jsx
+++ frontend/src/components/ApplicationForm/Experiences.jsx
@@ -1,6 +1,7 @@
 import useFormContext from "../../Hooks/useFormContext";
 import { useRef, useContext } from "react";
-import { Input } from "./index";
+import { Input, RequiredAstrik } from "./index";
+
 
 const Experiences = ({label, classes}) => {
     const message = useRef();
@@ -16,7 +17,7 @@ const Experiences = ({label, classes}) => {
             <Input type={'date'} fieldClasses="col-span-2" label="Expected to Graduate"/>
             <div id="TechnicalSkills" className={`flex flex-col grow ga-y-2 justify-between mb-4 md:my-0`}>
                 {/* <h2 className="text-lg mb-2">A message to the employer:-</h2> */}
-                <label className="text-md md:text-lg mb-2" htmlFor="TechnicalSkills">{label}:</label>
+                <label className="text-md md:text-lg mb-2" htmlFor="TechnicalSkills">{label}: <RequiredAstrik required={true} /></label>
                 <textarea placeholder="Include skills such as C++, Python - no need for explanations or ratings" onChange={getOption} ref={message} name="TechnicalSkills" id="TechnicalSkills" className="flex grow bg-transparent border border-gray-700 rounded-lg py-1.5 px-2 h-36 md:h-fit" ></textarea>
             </div>
         </div>
