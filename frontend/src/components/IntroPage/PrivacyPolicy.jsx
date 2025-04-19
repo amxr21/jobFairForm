75d3a01ed111a04f496e4a6ab30694d7ac281100
amxr21 <ammar211080@gmail.com>
2025-04-19 15:32:23 +0400

    Update PrivacyPolicy.jsx

--- frontend/src/components/IntroPage/PrivacyPolicy.jsx
+++ frontend/src/components/IntroPage/PrivacyPolicy.jsx
@@ -19,10 +19,10 @@ const PrivacyPopup = () => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[99999]">
-      <div className="bg-blue-600 text-white p-6 rounded-2xl max-w-md shadow-lg space-y-4">
+      <div className="bg-blue-600 text-white p-6 rounded-2xl max-w-xs md:max-w-md shadow-lg space-y-4">
         <h2 className="text-xl font-semibold">Privacy Notice</h2>
         <p className="text-sm">
-          This form collects your name, email, and ID solely for job fair participation purposes. 
+          This form collects your full name, email, phone number, and University ID solely for job fair participation purposes. 
           Your data will be securely stored and not shared with third parties.
         </p>
         <div className="flex justify-end">
